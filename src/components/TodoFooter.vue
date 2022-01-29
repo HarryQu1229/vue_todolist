6044d572d028cf78808b7c83d6e4fc924e353471
HarryQu1229 <Harryqu666@gmail.com>
2022-01-29 16:17:27 +1300

    created general layout and style

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo-footer">
+    <label>
+      <input type="checkbox" />
+    </label>
+    <span> <span>Accomplished 0</span> / 2 </span>
+    <button class="btn btn-danger">Clear Todos</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoFooter",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
